7fa148f8ec1442e67377f199f24519a08a23f26d
Kai Henseler <kai.henseler@strato.de>
2024-07-17 09:54:21 +0200

    feat(header): build to custom element
    Signed-off-by: Kai Henseler <kai.henseler@strato.de>

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,23 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [svelte()]
+	plugins: [
+		svelte({
+			compilerOptions: {
+				customElement: true
+			}
+		})
+	],
+	build: {
+		rollupOptions: {
+			output: {
+				entryFileNames: 'ionos-global-navigation.js'
+			}
+		},
+		lib: {
+			entry: 'src/App.svelte',
+			name: 'IonosGlobalNavigation',
+			formats: ['es']
+		}
+	}
 });

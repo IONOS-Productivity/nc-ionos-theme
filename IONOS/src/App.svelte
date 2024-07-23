389f2e8f30f4983812df93180ab46db6ca59f022
Kai Henseler <kai.henseler@strato.de>
2024-07-23 13:35:38 +0200

    dummy
    Signed-off-by: Kai Henseler <kai.henseler@strato.de>

--- IONOS/src/App.svelte
+++ IONOS/src/App.svelte
@@ -19,6 +19,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 <script lang="ts">
 	import Icon from './components/Icon.svelte';
+	console.log("Dummy")
 
 	export let home_src = '#';
 </script>

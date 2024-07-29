e367b29b862aa4117d4ef0307e6a6dfd96aa3c5f
Kai Henseler <kai.henseler@strato.de>
2024-07-29 16:23:15 +0200

    feat(header): provide basic translation
    Signed-off-by: Kai Henseler <kai.henseler@strato.de>

---
+++ core/l10n/de.js
@@ -0,0 +1,8 @@
+OC.L10N.register(
+    "core",
+    {
+    "IONOS WEBMAIL" : "IONOS WEBMAIL",
+    "Skip to main content" : "Zum Hauptinhalt springen",
+    "Skip to navigation of app" : "Zur Navigation der App springen"
+},
+"nplurals=2; plural=(n != 1);");

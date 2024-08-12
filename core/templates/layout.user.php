224dbce056036e8cf052a1f7a141db2d4d3881ef
Kai Henseler <kai.henseler@strato.de>
2024-08-12 09:52:27 +0200

    refactor(template/layout.user,ionos_header): use template for header
    Signed-off-by: Kai Henseler <kai.henseler@strato.de>

--- core/templates/layout.user.php
+++ core/templates/layout.user.php
@@ -74,21 +74,7 @@ p($theme->getTitle());
 			<?php if ($_['id-app-navigation'] !== null) { ?><a href="<?php p($_['id-app-navigation']); ?>" class="button primary skip-navigation"><?php p($l->t('Skip to navigation of app')); ?></a><?php } ?>
 		</div>
 
-		<header id="ionos-global-nav">
-			<ionos-global-nav
-			home_src="<?php p(\OC::$server->get(\OCP\IURLGenerator::class)->linkTo('', 'index.php'))?>">
-				<?php $link = \OC::$server->get(\OC\SystemConfig::class)->getValue("ionos_peer_products", [])['ionos_webmail_target_link']; ?>
-				<?php if ($link !== null) { ?>
-					<a href="<?php p($link) ?>"
-						target="_blank" slot="webmail"
-						title="<?php p($l->t('IONOS WEBMAIL')) ?>" data-qa="IONOS-WEBMAIL-TARGET">
-						<ionos-icons webmail />
-					</a>
-				<?php } ?>
-				<a href="<?php p(\OC_User::getLogoutUrl(\OC::$server->get(\OCP\IURLGenerator::class)))?>" style="color: var(--color-primary)">Logout</a>
-				<a href="<?php p(\OC::$server->get(\OCP\IURLGenerator::class)->linkToRoute('settings.PersonalSettings.index'))?>" style="color: var(--color-primary)">Settings</a>
-			</ionos-global-nav>
-		</header>
+		<?php print_unescaped($this->inc('custom/ionos_header')); ?>
 
 		<main id="content" class="app-<?php p($_['appid']) ?>">
 			<h1 class="hidden-visually" id="page-heading-level-1">

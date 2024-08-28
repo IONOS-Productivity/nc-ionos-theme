c89a3dfb6e822d8f04335b023d9b3ddceceea3a8
Kai Henseler <kai.henseler@strato.de>
2024-08-28 14:47:27 +0200

    fix(core/templates): remove unwanted webmail link
    Signed-off-by: Kai Henseler <kai.henseler@strato.de>

--- core/templates/layout.public.php
+++ core/templates/layout.public.php
@@ -48,14 +48,6 @@ p($theme->getTitle());
 	<header id="ionos-global-nav">
 		<ionos-global-nav
 		home_src="<?php p(\OC::$server->get(\OCP\IURLGenerator::class)->linkTo('', 'index.php'))?>">
-			<?php $link = \OC::$server->get(\OC\SystemConfig::class)->getValue("ionos_peer_products", [])['ionos_webmail_target_link']; ?>
-			<?php if ($link !== null) { ?>
-				<a href="<?php p($link) ?>"
-					target="_blank" slot="webmail"
-					title="<?php p($l->t('IONOS WEBMAIL')) ?>" data-qa="IONOS-WEBMAIL-TARGET">
-					<ionos-icons webmail />
-				</a>
-			<?php } ?>
 		</ionos-global-nav>
 	</header>
 

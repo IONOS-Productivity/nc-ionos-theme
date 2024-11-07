6bff1ed9cea737d825cb85bc41224150922387db
Kai Henseler <kai.henseler@strato.de>
2024-11-07 13:40:01 +0100

    fix: apply prettier format rules
    to scripts generated by the migration scripts
    
    Signed-off-by: Kai Henseler <kai.henseler@strato.de>

--- IONOS/src/components/Icon.svelte
+++ IONOS/src/components/Icon.svelte
@@ -36,7 +36,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 		user = false,
 		settings = false,
 		help = false,
-		logout = false
+		logout = false,
 	}: Props = $props();
 </script>
 

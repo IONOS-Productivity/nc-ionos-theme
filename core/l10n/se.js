bda48fa1e10bc967b06868d5ab45875ec540eca3
Kai Henseler <kai.henseler@strato.de>
2024-12-13 17:14:03 +0100

    chore(l10n): generate new translations
    Signed-off-by: Kai Henseler <kai.henseler@strato.de>

---
+++ core/l10n/se.js
@@ -0,0 +1,17 @@
+OC.L10N.register(
+    "core",
+    {
+    "Get your own free account" : "Skaffa ett eget gratiskonto",
+    "Go to %s" : "Gå till %s",
+    "Help & Support" : "Hjälp & Support",
+    "IONOS WEBMAIL" : "IONOS WEBMAIL",
+    "Logout" : "Logga ut",
+    "of {quota}" : "av {quota}",
+    "Search apps, files, tags, messages" : "Sök i filer och mappar",
+    "Settings" : "Inställningar",
+    "Skip to main content" : "Skippa till huvudinnehållet",
+    "Skip to navigation of app" : "Skippa till navigering av app",
+    "Storage used" : "Förvaring används",
+    "This application requires JavaScript for correct operation. Please {linkstart}enable JavaScript{linkend} and reload the page." : "Denna applikationen kräver JavaScript för att fungera korrekt. {linkstart}aktivera JavaScript{linkend} och uppdatera sidan."
+},
+"nplurals=2; plural=(n != 1);");

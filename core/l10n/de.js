afa9d31e3dd36249d01101e171419e65360e8c2e
Kai Henseler <kai.henseler@strato.de>
2024-12-13 17:14:03 +0100

    chore(l10n): generate new translations
    Signed-off-by: Kai Henseler <kai.henseler@strato.de>

--- core/l10n/de.js
+++ core/l10n/de.js
@@ -1,9 +1,17 @@
 OC.L10N.register(
     "core",
     {
+    "Get your own free account" : "Holen Sie sich Ihr eigenes kostenloses Konto",
+    "Go to %s" : "%s aufrufen",
+    "Help & Support" : "Hilfe & Support",
     "IONOS WEBMAIL" : "IONOS WEBMAIL",
+    "Logout" : "Abmelden",
+    "of {quota}" : "von {quota}",
+    "Search apps, files, tags, messages" : "Dateien und Ordner suchen",
+    "Settings" : "Einstellungen",
     "Skip to main content" : "Zum Hauptinhalt springen",
-    "Skip to navigation of app" : "Zur Navigation der App springen",
-    "Search apps, files, tags, messages": "Search files, folders",
+    "Skip to navigation of app" : "Zum Navigationsbereich der App springen",
+    "Storage used" : "Speicherverbrauch",
+    "This application requires JavaScript for correct operation. Please {linkstart}enable JavaScript{linkend} and reload the page." : "Diese Anwendung benötigt JavaScript zum ordnungsgemäßen Betrieb. Bitte {linkstart}aktivieren Sie JavaScript{linkend} und laden Sie die Seite neu."
 },
 "nplurals=2; plural=(n != 1);");

a4c5185b0044fe4ff67c5f469ad457cd240e9375
Misha M.-Kupriyanov <kupriyanov@strato.de>
2025-07-31 15:31:40 +0200

    fix: Disable auto-zoom on iOS
    When using iOS and focussing an input element the view should not be zoomed.
    So if we set a maximum scale iOS will not auto-zoom but still allow users to zoom.
    But we can not do this by default as this will disable user zoom on Chrome.
    
    https://github.com/nextcloud/server/commit/ec00367f
    
    Signed-off-by: Misha M.-Kupriyanov <kupriyanov@strato.de>

--- core/templates/layout.user.php
+++ core/templates/layout.user.php
@@ -44,7 +44,9 @@ p($theme->getTitle());
 ?>
 		</title>
 		<meta name="csp-nonce" nonce="<?php p($_['cspNonce']); /* Do not pass into "content" to prevent exfiltration */ ?>">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0<?php if (isset($_['viewport_maximum_scale'])) {
+			p(', maximum-scale=' . $_['viewport_maximum_scale']);
+		} ?>">
 
 		<?php $iosAppId = \OC::$server->get(\OC\SystemConfig::class)->getValue("ionos_customclient_ios_appid"); ?>
 		<?php if ($iosAppId !== '') { ?>

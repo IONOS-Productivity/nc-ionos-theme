14a705302f0b5730ceadc6c1b3db3bd174e2d00f
Misha M.-Kupriyanov <kupriyanov@strato.de>
2025-07-31 15:17:19 +0200

    feat: Provide CSP nonce as `<meta>` element
    This way we use the CSP nonce for dynamically loaded scripts.
    Important to notice: The CSP nonce must NOT be injected in `content` as
    this can lead to value exfiltration using e.g. side-channel attacts (CSS selectors).
    
    https://github.com/nextcloud/server/commit/2916e5df
    
    Signed-off-by: Misha M.-Kupriyanov <kupriyanov@strato.de>

--- core/templates/layout.user.php
+++ core/templates/layout.user.php
@@ -43,6 +43,7 @@ p(!empty($_['application']) ? $_['application'].' - ' : '');
 p($theme->getTitle());
 ?>
 		</title>
+		<meta name="csp-nonce" nonce="<?php p($_['cspNonce']); /* Do not pass into "content" to prevent exfiltration */ ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
 		<?php $iosAppId = \OC::$server->get(\OC\SystemConfig::class)->getValue("ionos_customclient_ios_appid"); ?>
